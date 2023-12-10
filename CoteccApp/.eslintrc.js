c819123a1a49d89e983ac4ad972f76ecd19950de
Loris Rossi <lorisrossirn@gmail.com>
2023-12-10 23:20:57 +0100

    chore: run linter and sort imports on pre-commit

--- CoteccApp/.eslintrc.js
+++ CoteccApp/.eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['import'],
+  // rules from https://manurana.medium.com/organizing-imports-in-react-and-react-native-faf982a3a3b5
+  rules: {
+    // this is for sorting WITHIN an import
+    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
