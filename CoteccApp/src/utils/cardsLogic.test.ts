85b0bf925e5616b34d45650291a2494480b91a13
Mauro Berlanda <mauro.berlanda@gmail.com>
2023-12-27 11:08:35 +0100

    test: fix unit tests

--- CoteccApp/src/utils/cardsLogic.test.ts
+++ CoteccApp/src/utils/cardsLogic.test.ts
@@ -25,7 +25,7 @@ describe('createDeck', () => {
   it('creates a deck with correct ranks', () => {
     const deck = createDeck();
     const ranks = new Set(deck.map(card => card.rank));
-    const expectedRanks = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const expectedRanks = new Set([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
     expect(ranks).toEqual(expectedRanks);
   });
 });
